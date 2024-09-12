28eb4160ed4ffbbed21171072a7cf78b3dc4fd60
steven <stevenmanuel1234@gmail.com>
2024-09-12 11:14:08 -0400

    .

--- Ejercicios/calculo.php
+++ Ejercicios/calculo.php
@@ -5,36 +5,23 @@ function calcularImpuesto(float $basePrice, float $taxPercentage): float
    return $basePrice * ($taxPercentage / 100);
     
 }
-//echo (calcularImpuesto("12.6" , "1.60")).  "\n";
 
 function calculateDiscount(float $basePrice, float $discountPercentage): float
 {
     return $basePrice * ($discountPercentage / 100);  
 }
-//echo (calculateDiscount("12.6" , "1.10")) .  "\n";
 
 function calculateTotal(float $basePrice , float $taxPercentage , float $discountPercentage) : float
 {
     $tax = calcularImpuesto($basePrice , $taxPercentage);
-    //tax = 45.88
     $discount = calculateDiscount ($basePrice, $discountPercentage);
-    //discount =44.94
+    
     return ($basePrice - $discount) + $tax;
 }
-//echo "total page: " . calcularImpuesto(45.3 , 1.30) .  "\n";
 echo "total page: " . calculateTotal (45.3 , 1.30 , 0.80) .  "\n";
 
-/*function main() 
-{
-    $baseprice = (float)readline("Introduce el precio base del producto: ");
-    $taxpercentage = (float)readline("Introduce el porcentaje de impuesto: ");
-    $discountpercentage = (float)readline("Introduce el porcentaje de descuento: ");
-    
-    $totalprice = calculateTotal($$baseprice, $taxpercentage, $discountpercentage);
-echo "El total a pagar es: " . number_format($totalprice, 2) . "\n";
-}
 
-main();*/
+
 
 
 /*Ejercicio Secuencial 4: (Cálculo de impuestos y descuentos)
--- POO/POO.php
+++ POO/POO.php
@@ -9,21 +9,17 @@ class Car {
         $this->ruedas = 4;
         $this->color = " "; 
         $this->motor = 1600;
-
     }
         function arrancar (){ 
             echo "estoy arrancando \n";
         }
-
         function girar (){ 
             echo "estoy girando \n";
         }
         function frenar () {
             echo "estoy frenando \n";
-
         }
 }
-
 $neon =new Car ();
 $mazda =new Car ();
 $corvette =new Car ();
